3225cc7ae5c4d5f6833b19623c581eb2a9a0b0a2
felpbv <65863976+felpbv@users.noreply.github.com>
2026-04-04 00:03:02 -0300

    Retornando para o small

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def gerar_embeddings(df):
     textos = df["informacao"].tolist()
 
     response = client.embeddings.create(
-        model="text-embedding-3-large",
+        model="text-embedding-3-small",
         input=textos
     )
 
